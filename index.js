9052e66dd581b654c255c87b39f4d13fc2deaeeb
pracniscate <lorena.giddens@outlook.com>
2018-11-07 17:29:43 -0800

    done

---
+++ index.js
@@ -0,0 +1,32 @@
+// calculates whether a trip is within range
+function produceDrivingRange(blockRange) {
+    return function(startingBlock, endingBlock) {
+        let start = parseInt(startingBlock);
+        let end = parseInt(endingBlock);
+        let distanceToTravel = Math.abs(end - start);
+        let difference = blockRange - distanceToTravel;
+
+        if (difference > 0) {
+            return `within range by ${difference}`;
+        } else {
+            return `${Math.abs(difference)} blocks out of range`;
+        };
+    };
+};
+
+function produceTipCalculator(percentage) {
+    return function(fare) {
+        return fare * percentage;
+    };
+};
+
+function createDriver() {
+    let driverId = 0;
+
+    return class {
+        constructor(name) {
+            this.id = ++driverId;
+            this.name = name;
+        };
+    };
+};
